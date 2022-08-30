2915f79ba34d5d39c0b2583c7a4ab862868a00b6
ashirogi-muto <kshitij.pandey@ac3.com.au>
2022-08-30 21:06:36 +1000

    - functions for setting and fetching rules from storage

--- background.js
+++ background.js
@@ -1,6 +1,43 @@
-let color = '#000'
+const RULE_STORAGE_KEY = "rules"
+const newRule = {
+	name: '',
+	url: ''
+}
+const tabRules = [
+	{
+		...newRule
+	}
+]
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color })
-  console.log('Default background color set to %cgreen', `color: ${color}`)
+chrome.runtime.onInstalled.addListener(async () => {
+	try {
+		await setRule(tabRules)
+		const rules = await getRules()
+		console.log(rules)
+	} catch (error) {
+		console.error(error)
+	}
 })
+
+
+const getRules = () => {
+	return new Promise((resolve, reject) => {
+		chrome.storage.sync.get([RULE_STORAGE_KEY], (result) => {
+			if(chrome.runtime.lastError) {
+				return reject(chrome.runtime.lastError)
+			}
+			resolve(result)
+		})
+	})
+}
+
+const setRule = (newRules) => {
+	return new Promise((resolve, reject) => {
+		chrome.storage.sync.set({[RULE_STORAGE_KEY]: newRules}, function() {
+			if(chrome.runtime.lastError) {
+				return reject(chrome.runtime.lastError)
+			}
+			resolve()
+		})
+	})
+}
--- popup.js
+++ popup.js
@@ -1,21 +1,13 @@
-let changeColor = document.getElementById("changeColor")
-
-chrome.storage.sync.get("color", ({ color }) => {
-	changeColor.style.backgroundColor = color
-})
-
-changeColor.addEventListener("click", async () => {
-	console.log("CLICK HERE")
-	let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-	console.log(tab)
-	chrome.scripting.executeScript({
-		target: { tabId: tab.id },
-		function: setPageBackgroundColor
-	})
-})
-
-const setPageBackgroundColor = () => {
-	chrome.storage.sync.get("color", ({ color }) => {
-		document.body.style.backgroundColor = color
-	})
+const newRule = {
+	name: '',
+	url: ''
 }
+const tabRules = [
+	{
+		...newRule
+	}
+]
+const addNewButton = document.getElementById("add-new-button")
+addNewButton.addEventListener("click", async () => {
+	tabRules.push({ ...newRule })
+})
